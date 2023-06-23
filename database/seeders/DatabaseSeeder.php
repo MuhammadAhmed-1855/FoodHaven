ec9bc09a247420e00a99ec4bf23ed89fdfdd17ee
MuhammadAhmed-1855 <i201855@nu.edu.pk>
2023-06-23 10:28:22 +0500

    Adding Sptie to dependancies

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +18,20 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user = User::create([
+            'name' => 'Eden Spell',
+            'email' => 'i201855@nu.edu.pk',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+
+        $adminRole = Role::create(['name' => 'admin']);
+        $driverRole = Role::create(['name' => 'driver']);
+        $customerRole = Role::create(['name' => 'customer']);
+        $cookRole = Role::create(['name' => 'cook']);
+
+        $user->assignRole($adminRole);
+        
     }
 }
